b534cae9cb033bc05d74d5010e67128b839a74ba
BliznyukS <bliznyuk.sergey@gmail.com>
2015-12-25 18:43:36 +0200

    DZ modul4 ADD Ceksius

--- src/modul41/Main.java
+++ src/modul41/Main.java
@@ -1,13 +1,21 @@
 package modul41;
-public class Main {
-    public static void main(String[] args){
-        int a = 3;
-        int b = 6;
-        int p = a*b;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        System.out.println("Ширина  "+a);
-        System.out.println("Высота  "+b);
-        System.out.println("");
-        System.out.println("Площадь прямоугольника  "+p);
+public class Main {
+    public static void main(String[] args)throws IOException {
+
+        double fahrenheit;
+        double celsius;
+        System.out.print("Temperature in Celsius: ");
+
+        BufferedReader bReader = new BufferedReader ( new InputStreamReader(System.in));
+        String cStr = bReader.readLine();
+        celsius = Double.parseDouble(cStr);
+        fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+        System.out.println(celsius + " Grad Celsius");
+        System.out.println(fahrenheit + " Grad Fahrenheit");
     }
 }
