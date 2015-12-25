08e07f926d422517ba6cb5c557fb316e0781fdb6
BliznyukS <bliznyuk.sergey@gmail.com>
2015-12-25 19:10:07 +0200

    DZ modul4 edit Circle

--- src/modul40/GetAreaCircle.java
+++ src/modul40/GetAreaCircle.java
@@ -6,7 +6,7 @@ public class GetAreaCircle {
 
     }
     public double calculateCircle(double pi, int radius){
-        double areaCircle = pi * Math.sqrt(radius);
+        double areaCircle = pi * Math.pow(radius,2);
         return areaCircle;
 
     }
