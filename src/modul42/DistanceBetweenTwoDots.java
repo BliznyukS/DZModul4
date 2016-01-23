759e9e7ce80f22df618363b3ce28626333af3740
BliznyukS <bliznyuk.sergey@gmail.com>
2016-01-23 19:26:07 +0200

    DZ4 Static

--- src/modul42/DistanceBetweenTwoDots.java
+++ src/modul42/DistanceBetweenTwoDots.java
@@ -6,8 +6,7 @@ import static java.lang.StrictMath.pow;
 
 public class DistanceBetweenTwoDots {
 
-
-    public double calculateByCoordinates(int xx1, int xx2, int yy1, int yy2) {
+    static double calculateByCoordinates(int xx1, int xx2, int yy1, int yy2) {
         double methotResult = sqrt(pow(abs(xx2 - xx1), 2) + pow(abs(yy2 - yy1), 2));
         return methotResult;
     }
