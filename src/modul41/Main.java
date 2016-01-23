5cc77776c83591445a4d6eecdfd7aa67705e4179
BliznyukS <bliznyuk.sergey@gmail.com>
2016-01-23 16:55:32 +0200

    DZ modul4 Вынес расчет градусов в отдельный класс 2

--- src/modul41/Main.java
+++ src/modul41/Main.java
@@ -1,24 +1,24 @@
 package modul41;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args)throws IOException {
         double resultDegree;
-
-/*      double fahrenheit;
         double celsius;
-        double result;
+
         System.out.print("Temperature in Celsius: ");
 
         BufferedReader bReader = new BufferedReader ( new InputStreamReader(System.in));
         String cStr = bReader.readLine();
         celsius = Double.parseDouble(cStr);
-        fahrenheit = (celsius * 9.0 / 5.0) + 32;
-*/
+
         DegreeConversion getDegreeConversion = new DegreeConversion();
         resultDegree = getDegreeConversion.convertCelsiusToFahrenheit();
 
-        //System.out.println(celsius + " Grad Celsius");
+        System.out.println(celsius + " Grad Celsius");
         System.out.println(resultDegree + " Grad Fahrenheit");
     }
 }
