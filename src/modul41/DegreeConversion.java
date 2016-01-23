6a7ffae16481340dd4029845a489af9c15c6b6e4
BliznyukS <bliznyuk.sergey@gmail.com>
2016-01-23 17:30:20 +0200

    DZ modul4  Ctrl+Alt+L

--- src/modul41/DegreeConversion.java
+++ src/modul41/DegreeConversion.java
@@ -2,11 +2,13 @@ package modul41;
 
 public class DegreeConversion {
     public double convertCelsiusToFahrenheit(double celsius) {
-        double fahrenheit;
+       /* double fahrenheit;
 
         fahrenheit = (celsius * 9.0 / 5.0) + 32;
 
-        return fahrenheit;
+        return fahrenheit;*/
+
+        return (celsius * 9.0 / 5.0) + 32;
     }
 
 }
