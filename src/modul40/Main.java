a91dd3366cafc2569e9577a9cdbe5ac5de144444
BliznyukS <bliznyuk.sergey@gmail.com>
2016-01-23 18:12:10 +0200

    DZ modul4  resultTriangleSquare

--- src/modul40/Main.java
+++ src/modul40/Main.java
@@ -11,9 +11,9 @@ public class Main {
         //double pi = 3.14;
         int radius = 7;
 
-        double resultTriangle;
-        double resultRectangle;
-        double resultCircle;
+        double resultTriangleSquare;
+        double resultRectangleSquare;
+        double resultCircleSquare;
         /*
         FigureSquare getAreaTriangle = new FigureSquare();
         resultTriangle = getAreaTriangle.calculateTriangle(sideT, heightT);
@@ -25,13 +25,13 @@ public class Main {
         resultCircle = getAreaCircle.calculateCircle(radius);
         */
         FigureSquare figureSquare = new FigureSquare();
-        resultTriangle = figureSquare.calculateTriangle(sideT, heightT);
-        resultRectangle = figureSquare.calculateRectangle(widthR, heightR);
-        resultCircle = figureSquare.calculateCircle(radius);
+        resultTriangleSquare = figureSquare.calculateTriangle(sideT, heightT);
+        resultRectangleSquare = figureSquare.calculateRectangle(widthR, heightR);
+        resultCircleSquare = figureSquare.calculateCircle(radius);
 
-        System.out.println(resultTriangle);
-        System.out.println(resultRectangle);
-        System.out.println(resultCircle);
+        System.out.println(resultTriangleSquare);
+        System.out.println(resultRectangleSquare);
+        System.out.println(resultCircleSquare);
 
     }
 }
